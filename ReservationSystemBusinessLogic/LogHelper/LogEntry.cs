6219e3cb3e1ec590c3a87a2c74a112bb6c89060b
Elad <41021512+ElBrenkel@users.noreply.github.com>
2021-04-13 21:38:08 +0300

    helper

---
+++ ReservationSystemBusinessLogic/LogHelper/LogEntry.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservationSystemBusinessLogic.Log
+{
+    [Table("ReservationSystemLog")]
+    public class LogEntry
+    {
+        public long Id { get; set; }
+
+        public DateTime Time { get; set; }
+
+        [StringLength(10)]
+        public string Level { get; set; }
+
+        [StringLength(100)]
+        public string Type { get; set; }
+
+        [StringLength(100)]
+        public string Method { get; set; }
+
+        [StringLength(1000)]
+        public string Message { get; set; }
+    }
+}
---
+++ ReservationSystemBusinessLogic/LogHelper/Logger.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using ReservationSystemBusinessLogic.Context;
+
+namespace ReservationSystemBusinessLogic.Log
+{
+    public static class Logger
+    {
+        static Logger()
+        {
+            using (ReservationLogContext logContext = new ReservationLogContext())
+            {
+                logContext.LogEntries.Create();
+            }
+        }
+
+        private static void Log(string level, string message)
+        {
+            (string type, string name) callerData = GetCallerData();
+            using (ReservationLogContext logContext = new ReservationLogContext())
+            {
+                logContext.LogEntries.Add(new LogEntry()
+                {
+                    Level = level,
+                    Time = DateTime.UtcNow,
+                    Type = callerData.type,
+                    Method = callerData.name,
+                    Message = message
+                });
+                logContext.SaveChanges();
+            }
+        }
+
+        public static void Debug(string message)
+        {
+            Log("DEBUG", message);
+        }
+
+        public static void Error(string message)
+        {
+            Log("ERROR", message);
+        }
+
+        private static (string type, string name) GetCallerData()
+        {
+            StackFrame frame = new StackFrame(3);
+            var method = frame.GetMethod();
+            var type = method.DeclaringType.Name;
+            var name = method.Name;
+            return (type, name);
+        }
+
+    }
+}
